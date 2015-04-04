4272a9a2cd078c8ef3d0b1803341c8338d17f097
tohich51 <tohich51@gmail.com>
2015-04-04 12:08:28 +0300

    Initial Commit

---
+++ LessonThree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LessonThree
+//
+//  Created by oscar on 04/04/15.
+//  Copyright (c) 2015 oscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
